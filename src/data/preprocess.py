b21a53b0bf76cf79ffdf35589818adfcaa2dc94d
jlawrence124 <jlawrence124@gmail.com>
2023-08-04 00:51:21 -0400

    updated to use conda and got mps check working

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,10 +1,8 @@
-import glob
-import os
-import numpy as np
+from pathlib import Path
+from typing import List
 import pandas as pd
 from dotenv import load_dotenv
-from typing import List
-from pathlib import Path
+from sentiment_analysis import analyze
 
 # Load the environment variables from the .env file
 load_dotenv()
@@ -23,23 +21,23 @@ def combine_csv_data(csv_files: List[str]) -> pd.DataFrame:
     """
     Combines all csv files into a single pandas dataframe.
     """
-    dfs = []
+    data_frame_list = []
     for csv_file in csv_files:
-        df = pd.read_csv(csv_file, encoding="ISO-8859-1")
-        dfs.append(df)
-    df = pd.concat(dfs, ignore_index=True)
-    return df
+        data_frame = pd.read_csv(csv_file, encoding="ISO-8859-1")
+        data_frame_list.append(data_frame)
+    data_frame = pd.concat(data_frame_list, ignore_index=True)
+    return data_frame
 
 
-def write_data_quality_text_file(df: pd.DataFrame):
+def write_data_quality_text_file(data_frame: pd.DataFrame):
     """
     Writes a text file with data quality information about the dataset.
     """
-    with open(Path("data/processed/data_quality.txt"), "w") as f:
-        f.write(f"Column names:\n{df.columns}\n\n")
-        f.write(f"Number of rows:\n{df.shape[0]}\n\n")
-        f.write(f"Number of null rows:\n{df.isnull().sum()}\n\n")
-        f.write(f"Number of duplicate rows:\n{df.duplicated().sum()}\n\n")
+    with open(Path("data/processed/data_quality.txt"), "w", encoding="utf-8") as file:
+        file.write(f"Column names:\n{data_frame.columns}\n\n")
+        file.write(f"Number of rows:\n{data_frame.shape[0]}\n\n")
+        file.write(f"Number of null rows:\n{data_frame.isnull().sum()}\n\n")
+        file.write(f"Number of duplicate rows:\n{data_frame.duplicated().sum()}\n\n")
 
         columns_to_remove_from_retweet_data = [
             "lang",
@@ -62,13 +60,16 @@ def write_data_quality_text_file(df: pd.DataFrame):
             "user_time_zone",
             "file",
         ]
-        f.write(
-            f"highest retweet count entries:\n{df.nlargest(5, 'retweet_count').drop(columns=columns_to_remove_from_retweet_data)}"
+        file.write(
+            f"""highest retweet count entries:\n
+            {data_frame.nlargest(5, 'retweet_count')
+            .drop(columns=columns_to_remove_from_retweet_data)
+            }"""
         )
-    f.close()
+    file.close()
 
 
-def write_data_quality_csv_file(df: pd.DataFrame):
+def write_data_quality_csv_file(data_frame: pd.DataFrame):
     """
     Writes a csv file with data quality information about the dataset.
     """
@@ -81,38 +82,37 @@ def write_data_quality_csv_file(df: pd.DataFrame):
                 "percentage that are retweets",
             ],
             "count": [
-                df.shape[0],
-                df.isnull().sum().sum(),
-                df.duplicated().sum(),
-                round((df[df["retweeted"] == "TRUE"].shape[0] / df.shape[0]) * 100, 2),
+                data_frame.shape[0],
+                data_frame.isnull().sum().sum(),
+                data_frame.duplicated().sum(),
+                round(
+                    (
+                        data_frame[data_frame["retweeted"] == "TRUE"].shape[0]
+                        / data_frame.shape[0]
+                    )
+                    * 100,
+                    2,
+                ),
             ],
         }
     )
     csv_path = Path("data/processed/statistics.csv")
     statistics_df.to_csv(csv_path, index=False)
 
-def get_raw_tweet_text_data(df: pd.DataFrame) -> List[str]:
-    return df['text'].tolist()
 
 def preprocess_data():
     """
     Calls other functions to preprocess the data.
     """
     csv_list = get_csv_files()
-    
+
     combined_data_frame = combine_csv_data(csv_list)
     write_data_quality_text_file(combined_data_frame)
     write_data_quality_csv_file(combined_data_frame)
 
     print(combined_data_frame.head(5))
     print(combined_data_frame.shape)
-    raw_tweet_list = get_raw_tweet_text_data(combined_data_frame)
-
-    # Write the raw tweet data to a text file or create it if it doesn't exist
-    with open(Path("data/processed/tweets.txt"), "w") as f:
-        for tweet in raw_tweet_list:
-            f.write(f"{tweet}\n\n")
-    f.close()
+    analyze(combined_data_frame)
 
 
 if __name__ == "__main__":
--- src/data/sentiment_analysis.py
+++ src/data/sentiment_analysis.py
@@ -1,18 +1,72 @@
-from transformers import pipeline
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+import pandas as pd
+import torch
+from tqdm import tqdm
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
-sentiment_analyzer = pipeline("sentiment-analysis")
+# Check if MPS is available
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+    print("Using MPS (Metal GPU) device.")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using CUDA device.")
+else:
+    device = torch.device("cpu")
+    print("Using CPU device.")
+
+tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
+MODEL_NAME = AutoModelForSequenceClassification.from_pretrained(
+    "cardiffnlp/twitter-roberta-base-sentiment"
+)
+
+sentiment_analyzer = pipeline(
+    "text-classification",
+    model=MODEL_NAME,
+    tokenizer=tokenizer,
+    device=device
+)
 
 
+@dataclass
 class Sentiment:
-    def __init__(self, score: float, label: str, brand_risk: bool = False):
-        self.score = score
-        self.label = label
-        self.brand_risk = brand_risk
+    """
+    Sentiment object
+    """
+
+    score: float
+    label: str
+    brand_risk: bool = False
 
 
-def analyze(text: str):
-    result = sentiment_analyzer(text)
-    for item in result:
-        sentiment = Sentiment(score=item["score"], label=item["label"])
+def analyze(data_frame: pd.DataFrame, batch_size=50) -> List[Sentiment]:
+    """
+    Analyzes the sentiment of a given text by a set batch size.
+    """
+    results = []
+    raw_tweets_text = get_raw_tweet_text_data(data_frame)
+    for i in tqdm(
+        range(0, len(raw_tweets_text), batch_size), desc="Analyzing sentiments"
+    ):
+        batch = raw_tweets_text[i : i + batch_size]
+        result = sentiment_analyzer(batch)
+        results.extend(result)
+    return results
 
-    return sentiment
+
+def get_raw_tweet_text_data(data_frame: pd.DataFrame) -> List[str]:
+    """
+    Get all the text data for all tweets.
+    """
+    return data_frame["text"].tolist()
+
+
+def write_sentiment_to_csv(data_frame: pd.DataFrame):
+    """
+    Writes a csv file with sentiment analysis of the dataset.
+    """
+
+    data_frame["sentiment"] = data_frame["text"].apply(analyze)
+    data_frame.to_csv(Path("data/processed/sentiment.csv"), index=False)
